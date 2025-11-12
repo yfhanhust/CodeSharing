29226842d309b5d06f91c45c91f19cce8fca6f2e
Yufei HAN <yfhan.hust@gmail.com>
2025-11-12 06:47:16 +0100

    Update test_mistral_small.py

--- code/test_mistral_small.py
+++ code/test_mistral_small.py
@@ -13,8 +13,8 @@ MAX_TOKEN_COUNT = 500  # Adjust this value as needed
 MAX_CHAR_COUNT = 20000
 from mistralai import Mistral
 
-api_key = "MsfxS4rUqSbcPy05FqpTeozsFN5RpOuH"
-model = "open-mistral-7b"
+api_key = "your API key"
+model = "your LLM choice"
 
 client = Mistral(api_key=api_key)
 
@@ -32,17 +32,8 @@ system_prompt = ("As an expert malware analyst, your task is to analyze the foll
     "9.Malware scripts may not have error handling. The absence of error handling in scripts can indicate malicious intent, as legitimate scripts typically include error handling to manage potential issues.\n"
     "### Output Requirements:\n"
     "In your output, you SHOULD output ‘Malware’ or ‘Not Malware’ to clearly state your detection. Afterwards, you should clearly justify your classification decision using bullet points:\n"
-    #"If you assess the script as malicious, start with: Reasons to detect as malware.\n"
-    #"If you determine the script is benign or uncertain, begin with: Reasons to detect NOT as malware.\n"
     "PowerShell Script:")
 
-#system_prompt = ("As an expert malware analyst, your task is to analyze the following PowerShell script and classify it as either benign, malicious, or uncertain.\n"
-#                 "### Output Requirements:\n"
-#                 "In your output, you SHOULD output ‘Malware’ or ‘Not malware’ to clearly state your detection.\n"
-#                 #"If you assess the script as malicious, start with: Reasons to detect as malware.\n"
-#                 #"If you determine the script is benign, begin with: Reasons to detect NOT as malware.\n"
-#                 "PowerShell Script:"
-#                 )
 
 def check_file_for_malicious_code(file_path):
     # Read the content of the file
@@ -56,8 +47,6 @@ def check_file_for_malicious_code(file_path):
     # Check if the file exceeds the maximum character count
     if len(code_content) > MAX_CHAR_COUNT:
         return "Skipped due to excessive length", f"File has more than {MAX_CHAR_COUNT} characters", ""
-
-    # Send the content to the Vertex AI API
 
 
     try:
@@ -74,28 +63,20 @@ def check_file_for_malicious_code(file_path):
     except ValueError as e:
         response = "Response blocked by safety filters."
     except Exception as e:
-        # Log quota errors and other unexpected exceptions
         response = "Quota exceeded or other error"
-        #with open('quota_errors.log', 'a') as quota_log:
-        #    quota_log.write(f"{datetime.now()} - Error for file: {file_path}\n{str(e)}\n")
-
+       
     # Evaluate the response for malicious content indication
     if "Not sure" in response.choices[0].message.content or "Uncertain" in response.choices[0].message.content or "Not malware" in response.choices[0].message.content:
         result = 0
     else:
         result = 1
 
-    # Extracting a sumistral-large-latestmmary about the code's functionality
-    #summary_start = response_text.find('Summary:') + len('Summary:')
-    #summary = response_text[summary_start:].strip()
 
     return result, response
-
-#XIZYm8hIfL5a51keQ1WhDXXErpuzCvfh
-#mistral-large-latest
+    
 def test_file_path(directory, file_name_set):
     for filename in file_name_set:
-        file_set_path = os.path.isfile('/home/yhan/Documents/research/code_attack-main/preprocessed_ps1malware/'+filename[:-1])
+        file_set_path = os.path.isfile('/preprocessed_ps1malware/'+filename[:-1])
         print(file_set_path)
 
 
@@ -106,7 +87,7 @@ def process_files(directory, file_name_set, results_filename):
         # Process each file in the directory
         #for filename in os.listdir(directory):
         for filename in file_name_set:
-            file_set_path = '/home/yhan/Documents/research/code_attack-main/preprocessed_ps1malware/' + filename[:-1]
+            file_set_path = '/preprocessed_ps1malware/' + filename[:-1]
             #print(file_set_path)
             print(os.path.isfile(file_set_path))
             if os.path.isfile(file_set_path):
@@ -116,16 +97,9 @@ def process_files(directory, file_name_set, results_filename):
                 except Exception as e:
                     malicious_result, response_text = "Error", str(e)
 
-                # Write the results to the file
                 results_file.write(
                     f"File: {filename}\nMalicious: {malicious_result}\nResponse: {response_text}\n\n")
                 results_file.flush()  # Ensure the data is written to the file
-                # Log the request
-                #log_file.write(f"{datetime.now()} - Processed file: {filename}\n")
-                #log_file.flush()
-                # Print the result for debugging
-                # print(f"Processed file: {filename}\nMalicious: {malicious_result}\nSummary: {summary}\nResponse: {response_text}\n")
-                # Add a delay to avoid exceeding quota
                 end_time = datetime.now()
                 elapsed_time = (end_time - start_time).total_seconds()
                 sleep_time = max(30 - elapsed_time, 0)  # Adjust the sleep time
@@ -135,7 +109,7 @@ def process_files(directory, file_name_set, results_filename):
 if __name__ == "__main__":
     # extract the file hashes for the testing samples
     file_hashes = []
-    with open('/home/yhan/PycharmProjects/pythonProject/mistral_12b_summary.txt','r') as f:
+    with open('/summary.txt','r') as f:
         for one_line in f:
             if 'File: ' in one_line:
                 file_hashes.append(one_line[6:])
@@ -143,12 +117,10 @@ if __name__ == "__main__":
     print(1/3*len(file_hashes))
 
     # Directory containing the code files
-    directory = "/home/yhan/Documents/research/code_attack-main/preprocessed_ps1malware/"
+    directory = "/preprocessed_ps1malware/"
 
     # Filename for the results
-    results_filename = "/home/yhan/PycharmProjects/pythonProject/mistral_7b_test_result_promptinject.txt"
-    #log_filename = "request_log.txt"
+    results_filename = "test_result.txt"
 
-    process_files(directory, file_hashes[484:], results_filename)
-    #test_file_path(directory, file_hashes[484:])
-    print("Processing complete. Results are saved in mistral_7B_results.txt")
+    process_files(directory, file_hashes, results_filename)
+    print("Processing complete. Results are saved in tset_result.txt")
